838da2b5c396282f2ca5eac6c0a82af03095ea0f
Rubel Talukder <talukderrubel223@gmail.com>
2022-02-28 23:04:15 +0600

    phone display added

--- js/app.js
+++ js/app.js
@@ -3,8 +3,26 @@ const searchPhone=()=>{
   const url=`https://openapi.programming-hero.com/api/phones?search=${phoneBox}`
   fetch(url)
   .then(res=>res.json())
-  .then(data=>console.log(data.data))
+  .then(data=>displayPhoneItems(data.data))
 }
 const displayPhoneItems=(phones)=>{
   console.log(phones)
+  const parentPhoneArea=document.getElementById('phone-area')
+  for(const phone of phones){
+    // console.log(phone)
+    const div=document.createElement("div")
+    div.classList.add('col')
+    div.innerHTML=`
+    <div class="card h-100 w-75 text-center">
+        <img  src="${phone.image}" class="card-img-top p-3 " alt="...">
+        <div class="card-body mx-auto">
+          <h4 class="card-title">${phone.phone_name}</h4>
+          <h5 class="card-text">${phone.brand}</h5>
+          <button onclick="phoneDeatails('${phone.}')" class="btn btn-primary mx-auto">Deatails</button>
+        </div>
+      </div>
+    
+    `
+    parentPhoneArea.appendChild(div)
+  }
 }
