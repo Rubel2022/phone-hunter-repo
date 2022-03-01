5cde3d71dbbbf3860d2abda70804dc3451ab878c
Rubel Talukder <talukderrubel223@gmail.com>
2022-03-01 14:35:01 +0600

    data error handale

--- js/app.js
+++ js/app.js
@@ -6,30 +6,41 @@ const searchPhone=()=>{
   const url=` https://openapi.programming-hero.com/api/phones?search=${phoneBox}`
   fetch(url)
   .then(res=>res.json())
-  .then(data=>displayPhoneItems(data.data))
+  .then(data=>displayPhoneItems(data.data.slice(0,20)))
 }
 const displayPhoneItems=(phones)=>{
   // console.log(phones)
-  const parentPhoneArea=document.getElementById('phone-area')
-  for(const phone of phones){
-    console.log(phone)
-      const div=document.createElement("div")
-      div.classList.add('col')
-      div.innerHTML=`
-      <div class="card h-100 w-75 text-center mx-auto">
-          <img  src="${phone.image}" class="card-img-top p-3 " alt="...">
-          <div class="card-body mx-auto">
-            <h4 class="card-title">${phone.phone_name}</h4>
-            <h5 class="card-text">${phone.brand}</h5>
-            <button onclick="phoneDeatails('${phone.slug}')" class="btn btn-primary mx-auto">Deatails</button>
-          </div>
-        </div>
-      
-      `
-      parentPhoneArea.appendChild(div)
-    }
-   
+  if(phones.length===0){
+    document.getElementById("error").style.display="block"
+    
+    
   }
+    
+      else{
+        const parentPhoneArea=document.getElementById('phone-area')
+        for(const phone of phones){
+          // console.log(phone)
+            const div=document.createElement("div")
+            document.getElementById("error").style.display="none"
+            div.classList.add('col')
+            div.innerHTML=`
+            <div class="card h-100 w-75 text-center mx-auto">
+                <img  src="${phone.image}" class="card-img-top p-3 " alt="...">
+                <div class="card-body mx-auto">
+                  <h4 class="card-title">${phone.phone_name}</h4>
+                  <h5 class="card-text">${phone.brand}</h5>
+                  <button onclick="phoneDeatails('${phone.slug}')" class="btn btn-primary mx-auto">Deatails</button>
+                </div>
+              </div>
+            
+            `
+            parentPhoneArea.appendChild(div)
+          }
+      }
+  }
+  
+   
+  
 
 const phoneDeatails=(deatails)=>{
   document.getElementById('phone-deatails').innerHTML=""
@@ -40,25 +51,22 @@ const phoneDeatails=(deatails)=>{
   .then(data=>displayPhoneDeatails(data.data))
 }
 const displayPhoneDeatails=(deatails)=>{
-  console.log(deatails)
+  console.log(deatails.releaseDate)
 const deatailsArea=document.getElementById('phone-deatails')
 const div=document.createElement('div')
 div.classList.add('col')
 div.innerHTML=`
-<div class="card h-100 w-75 text-center mx-auto">
-<img  src="${deatails.image}" class="card-img-top p-5 " alt="...">
-<div class="card-body mx-auto p-4">
+<div class="card h-100 mx-auto ">
+<img   src="${deatails.image}" class="card-img-top" alt="...">
+<div class="card-body text-center p-4">
   <h4 class="card-title text-primary">${deatails.name}</h4>
   <h5 class="card-text">${deatails.brand}</h5>
-  <p> ${deatails.releaseDate}</p>
+  <p> ${(deatails.releaseDate !=="") ? deatails.releaseDate:('no-released')}</p>
   <h6>${deatails.mainFeatures.storage}</h6>
   <h6>${deatails.mainFeatures.memory}</h6>
   <h6>${deatails.mainFeatures.displaySize}</h6>
   <h6>${deatails.mainFeatures.chipSet}</h6>
-  <button onclick="sensorbtn('${deatails.mainFeatures.sensors}')">Sensor</button>
-    
-  
-  
+  <h6>Sensore:${deatails.mainFeatures.sensors}</h6> 
 </div>
 </div>
 `
@@ -66,10 +74,4 @@ deatailsArea.appendChild(div)
 
 }
 
-const sensorbtn=(sensors)=>{
-  console.log(sensors)
-  const phoneSensore=document.getElementById('phone-sensore')
-   phoneSensore.innerHTML=`
-   <h2>${sensors}</h2>
-  `
-}
+
